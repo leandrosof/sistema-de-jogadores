6bd94787b95c97e2ac4da7bdf41e9692f4cd32cb
leandrosof <leandrosof@users.noreply.github.com>
2025-04-22 18:12:40 +0000

    deploy: e9f5d8f3c3d80f853e960b883a6a8dea214f5866

---
+++ _next/static/chunks/app/layout-019a2d4d8b599488.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{4521:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,9324,23)),Promise.resolve().then(n.t.bind(n,5685,23)),Promise.resolve().then(n.t.bind(n,3063,23)),Promise.resolve().then(n.t.bind(n,4697,23))},9324:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[533,740,188,283,441,684,358],()=>s(4521)),_N_E=e.O()}]);
